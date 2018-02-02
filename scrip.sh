23d4096d7703e4e9c2054945d8348142f08d4e60
Gabriella Mosca <mosca@mpipz.mpg.de>
2018-02-02 16:41:59 +0100

    A testof differences and conflicts

--- scrip.sh
+++ scrip.sh
@@ -7,3 +7,4 @@ mkdir fastQCResults
 /home/student/software/FastQC/fastqc -t 4 -o fastQCResults /home/student/Documents/bashScripting/data/ERR000064_200_2.fastq
 /home/student/software/FastQC/fastqc -t 4 -o fastQCResults /home/student/Documents/bashScripting/data/ERR000061_500_1.fastq
 /home/student/software/FastQC/fastqc -t 4 -o fastQCResults /home/student/Documents/bashScripting/data/ERR000061_500_2.fastq
+Adding random bullshit
